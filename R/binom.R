db72d62ab1c7f78bff60a45ddd70b6791ca6b227
Danny Morrs <usc00@hotmail.com>
2019-04-07 23:39:45 -0400

    binomial

--- R/binom.R
+++ R/binom.R
@@ -1,26 +1,26 @@
 
-trials <- 100
-
-p <- 0.05
-q <- 0.95
-
-dbinom(0, size=100, prob=0.00125)
-
-pbinom(1, size=100, prob=0.00125, lower.tail = F)
-
-
+#' Cumulative Binomial Probability
+#'
+#' @param n_success cumulative number of successes to calculate
+#' @param prob_success probability of a single success
+#' @param sample_size size of the sample (number of trials)
+#' @importFrom dplyr %>%
+#' @examples
+#' cumul_binom_prob(n_success = 10, prob_success = 0.5, sample_size = 20)
+#' @export
 cumul_binom_prob <- function(n_success, prob_success, sample_size) {
   lower_tails <- c(lower_tail = TRUE,
                    upper_tail = FALSE)
 
   results <- purrr::map(lower_tails, function(x) {
+
     pbinom(q = n_success, size = sample_size,  prob = prob_success,
            lower.tail = x)
   }) %>%
     tibble::as_tibble() %>%
-    mutate(n_success = n_success,
-           prob_success = prob_success,
-           sample_size = sample_size)
+    dplyr::mutate(n_success = n_success,
+                  prob_success = prob_success,
+                  sample_size = sample_size)
 
   explanation <- c(
     paste("The probability of", n_success, "or fewer successes is", round(results$lower_tail, 4)),
@@ -32,12 +32,22 @@ cumul_binom_prob <- function(n_success, prob_success, sample_size) {
 }
 
 
+
+#' Binomial Probability
+#'
+#' @param n_success number of successes to calculate
+#' @param prob_success probability of a single success
+#' @param sample_size size of the sample (number of trials)
+#' @importFrom dplyr %>%
+#' @examples
+#' binom_prob(n_success = 10, prob_success = 0.5, sample_size = 20)
+#' @export
 binom_prob <- function(n_success, prob_success, sample_size) {
 
   results <- dbinom(x = n_success, size = sample_size,  prob = prob_success) %>%
-    tibble::as_tibble() %>%
-    rename(prob = value) %>%
-    mutate(n_success = n_success,
+    tibble::enframe(name = NULL) %>%
+    dplyr::rename(prob = value) %>%
+    dplyr::mutate(n_success = n_success,
            prob_success = prob_success,
            sample_size = sample_size)
 
